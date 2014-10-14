2cefd1808848123c8fd75805df5b7227aafb8760
Ran <liran319@gmail.com>
2014-10-14 13:57:09 +0800

    修改bloglist为可以显示链接

--- blog/sblog/urls.py
+++ blog/sblog/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls import *
 urlpatterns = patterns(('sblog.views'),
     url(r'^bloglist/$', 'blog_list', name='bloglist'),
     # name属性是给这个url起个别名，可以在模版中引用而不用担心urls文件中url的修改 引用方式为{% url bloglist %}
+    url(r'^blog/(?P<id>\d+)/$', 'blog_show', name='detailblog'),
 )
--- blog/sblog/views.py
+++ blog/sblog/views.py
@@ -3,8 +3,17 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from sblog.models import Blog
+from django.http import Http404
 
 
 def blog_list(request):
     blogs = Blog.objects.all()
     return render_to_response("blog_list.html", {"blogs": blogs})
+
+
+def blog_show(request, id=''):
+    try:
+        blog = Blog.objects.get(id=id)
+    except Blog.DoesNotExist:
+        raise Http404
+    return render_to_response("blog_show.html", {"blog": blog})
